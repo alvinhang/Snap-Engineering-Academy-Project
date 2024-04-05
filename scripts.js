c22e74ba89892b8226962a75dc70dc254e731558
Alvin <alvinlhang@gmail.com>
2024-04-04 21:24:41 -0700

    Testing Car Catalog from Starter Code

--- scripts.js
+++ scripts.js
@@ -24,60 +24,72 @@
  */
 
 
-const FRESH_PRINCE_URL = "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL = "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL = "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
-
-// This is an array of strings (TV show titles)
-let titles = [
-    "Fresh Prince of Bel Air",
-    "Curb Your Enthusiasm",
-    "East Los High"
-];
+const carMakes = {
+    "Audi": "https://upload.wikimedia.org/wikipedia/commons/9/92/Audi-Logo_2016.svg",
+    "BMW": "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/BMW.svg/1015px-BMW.svg.png",
+    "Cheverolet": "https://di-uploads-pod2.dealerinspire.com/biggerschevy/uploads/2018/02/2013-Chevrolet-BowTie.jpg",
+    "Dodge": "https://upload.wikimedia.org/wikipedia/commons/6/6a/Dodge_logo.svg",
+    "Ford": "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Ford_logo_flat.svg/1200px-Ford_logo_flat.svg.png",
+    "Honda": "https://upload.wikimedia.org/wikipedia/commons/3/38/Honda.svg",
+    "Jeep": "https://upload.wikimedia.org/wikipedia/commons/0/0d/Jeep_logo.svg",
+    "Mazda": "https://upload.wikimedia.org/wikipedia/commons/3/3f/Mazda_logo.svg",
+    "Mercedes": "https://upload.wikimedia.org/wikipedia/commons/9/90/Mercedes-Logo.svg",
+    "Nissan": "https://upload.wikimedia.org/wikipedia/commons/2/23/Nissan_2020_logo.svg",
+    "Porsche": "https://upload.wikimedia.org/wikipedia/de/7/70/Porsche_Logo.svg",
+    "Subaru": "https://upload.wikimedia.org/wikipedia/commons/4/47/Subaru_logo.svg",
+    "Tesla": "https://upload.wikimedia.org/wikipedia/commons/b/bd/Tesla_Motors.svg",
+    "Toyota": "https://upload.wikimedia.org/wikipedia/commons/e/e7/Toyota.svg",
+    "Volkswagen": "https://upload.wikimedia.org/wikipedia/commons/6/6d/Volkswagen_logo_2019.svg"
+};
 // Your final submission should have much more data than this, and 
 // you should use more than just an array of strings to store it all.
 
 
-// This function adds cards the page to display the data in the array
-function showCards() {
-    const cardContainer = document.getElementById("card-container");
-    cardContainer.innerHTML = "";
-    const templateCard = document.querySelector(".card");
+
+function createCard(title, imageURL) {
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const cardContent = document.createElement("div");
+    cardContent.classList.add("card-content");
+
+    const cardTitle = document.createElement("h2");
+    cardTitle.textContent = title;
+
+    const image = document.createElement("img");
+    image.src = imageURL;
+    image.alt = title + " Logo";
+
+    cardContent.appendChild(cardTitle);
+    cardContent.appendChild(image);
+    card.appendChild(cardContent);
+
+    return card;
+}
+
+function addCard() {
+    const title = prompt("Enter the car make:");
+    const imageURL = prompt("Enter the image URL for the car make:");
     
-    for (let i = 0; i < titles.length; i++) {
-        let title = titles[i];
-
-        // This part of the code doesn't scale very well! After you add your
-        // own data, you'll need to do something totally different here.
-        let imageURL = "";
-        if (i == 0) {
-            imageURL = FRESH_PRINCE_URL;
-        } else if (i == 1) {
-            imageURL = CURB_POSTER_URL;
-        } else if (i == 2) {
-            imageURL = EAST_LOS_HIGH_POSTER_URL;
-        }
-
-        const nextCard = templateCard.cloneNode(true); // Copy the template card
-        editCardContent(nextCard, title, imageURL); // Edit title and image
-        cardContainer.appendChild(nextCard); // Add new card to the container
+    if (title && imageURL) {
+        // Add the new car make to the carMakes object
+        carMakes[title] = imageURL;
+        
+        // Refresh the displayed cards
+        showCards();
+    } else {
+        alert("Please provide both a title and an image URL.");
     }
 }
 
-function editCardContent(card, newTitle, newImageURL) {
-    card.style.display = "block";
+function showCards() {
+    const cardContainer = document.getElementById("card-container");
+    cardContainer.innerHTML = "";
 
-    const cardHeader = card.querySelector("h2");
-    cardHeader.textContent = newTitle;
-
-    const cardImage = card.querySelector("img");
-    cardImage.src = newImageURL;
-    cardImage.alt = newTitle + " Poster";
-
-    // You can use console.log to help you debug!
-    // View the output by right clicking on your website,
-    // select "Inspect", then click on the "Console" tab
-    console.log("new card:", newTitle, "- html: ", card);
+    for (const [title, imageURL] of Object.entries(carMakes)) {
+        const nextCard = createCard(title, imageURL); // Create card for the current car make
+        cardContainer.appendChild(nextCard); // Add new card to the container
+    }
 }
 
 // This calls the addCards() function when the page is first loaded
@@ -89,6 +101,6 @@ function quoteAlert() {
 }
 
 function removeLastCard() {
-    titles.pop(); // Remove last item in titles array
+    delete carMakes[Object.keys(carMakes).pop()]; // Remove last item in titles array
     showCards(); // Call showCards again to refresh
 }
